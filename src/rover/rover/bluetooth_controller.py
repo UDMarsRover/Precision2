25fb998c7c3899184fb04df4ab1ce300313fe040
Ryan Kuederle <ryan@rkuederle.com>
2024-09-15 18:17:59 -0400

    Automatically find controller

--- src/rover/rover/bluetooth_controller.py
+++ src/rover/rover/bluetooth_controller.py
@@ -1,13 +1,20 @@
 #import evdev
 from evdev import InputDevice, categorize, ecodes
 
-#creates object 'gamepad' to store the data
-#you can call it whatever you like
-gamepad = InputDevice('/dev/input/event0')
+# Find and create device
+devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+for device in devices:
+    if 'Controller' in device.name:
+        print('Found controller:', device.name)
+        # Additional code for handling the controller can be added here
+    print(device.path, device.name, device.phys)
+    gamepad = InputDevice(device.path)
+    
 
 #prints out device info at start
 print(gamepad)
 
 #evdev takes care of polling the controller in a loop
 for event in gamepad.read_loop():
-    print(categorize(event))
+    if event.type == evdev.ecodes.EV_KEY:
+        print(categorize(event))

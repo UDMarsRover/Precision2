92f5cae71abf2fefdbf6abb040adc10028452238
Ryan Kuederle <ryan@rkuederle.com>
2024-09-15 17:13:09 -0400

    Updated MAC

--- src/rover/rover/bluetooth_controller_setup.sh
+++ src/rover/rover/bluetooth_controller_setup.sh
@@ -13,14 +13,12 @@ EOF
 sleep 5
 
 # Attempt to connect to the device
-bluetoothctl connect <DEVICE_MAC_ADDRESS>
+bluetoothctl connect 98:7A:14:89:A6:F8
 
 # Check if the device is connected
-if bluetoothctl info <DEVICE_MAC_ADDRESS> | grep -q "Connected: yes"; then
+if bluetoothctl info 98:7A:14:89:A6:F8 | grep -q "Connected: yes"; then
     echo "Device connected successfully."
 else
     echo "Device not found. Exiting."
     exit 1
 fi
-
-# 98:7A:14:89:A6:F8

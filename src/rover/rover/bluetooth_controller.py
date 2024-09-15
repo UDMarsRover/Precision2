c6447cea4fb6d333d5c4d4470cb92b3a43a55ea1
Ryan Kuederle <ryan@rkuederle.com>
2024-09-15 18:35:21 -0400

    Processed stick value

--- src/rover/rover/bluetooth_controller.py
+++ src/rover/rover/bluetooth_controller.py
@@ -1,6 +1,9 @@
 #import evdev
 from evdev import InputDevice, categorize, ecodes
 import evdev
+
+CENTER_TOLERANCE = 350
+STICK_MAX = 65536
 # Find and create device
 devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 for device in devices:
@@ -13,6 +16,14 @@ for device in devices:
 
 #prints out device info at start
 print(gamepad)
+
+def get_stick_value(raw_value):
+    if raw_value > CENTER_TOLERANCE:
+        return (raw_value - CENTER_TOLERANCE) / (STICK_MAX - CENTER_TOLERANCE)
+    elif raw_value < -CENTER_TOLERANCE:
+        return (raw_value + CENTER_TOLERANCE) / (STICK_MAX - CENTER_TOLERANCE)
+    else:
+        return 0
 
 #evdev takes care of polling the controller in a loop
 for event in gamepad.read_loop():
@@ -27,4 +38,6 @@ for event in gamepad.read_loop():
         # elif absevent.event.code == ecodes.ABS_RX:
         #     print("Right Stick X:", absevent.event.value)
         if absevent.event.code == 2:
-            print("Right Stick Y:", absevent.event.value)
+            
+            print("Right Stick Y:", get_stick_value(absevent.event.value))
+

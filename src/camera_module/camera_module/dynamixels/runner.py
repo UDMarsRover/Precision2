ab812cd93d37935f3466d1171bbfc6d0a335ae0f
Ryan Kuederle <ryankuederle@icloud.com>
2024-10-27 18:52:29 -0400

    Changed runner timing

--- src/camera_module/camera_module/dynamixels/runner.py
+++ src/camera_module/camera_module/dynamixels/runner.py
@@ -4,5 +4,6 @@ import time
 motor = DynamixelMX('/dev/ttyAMA0', 1, 57600)
 while True:
     motor.write_goal_position(2048)
-    time.sleep(0.5)
+    time.sleep(1.5)
     motor.write_goal_position(0)
+    time.sleep(1.5)

ed4d8008790ee73287ca835f87b660c672d297d9
Ryan Kuederle <ryankuederle@icloud.com>
2024-10-27 18:57:19 -0400

    Velocity mode test

--- src/camera_module/camera_module/dynamixels/runner.py
+++ src/camera_module/camera_module/dynamixels/runner.py
@@ -2,8 +2,9 @@ from control import DynamixelMX
 import time
 
 motor = DynamixelMX('/dev/ttyAMA0', 1, 57600)
+motor.set_velocity_mode()
 while True:
-    motor.write_goal_position(8000)
+    motor.write_goal_velocity(10)
     time.sleep(2)
-    motor.write_goal_position(0)
+    motor.write_goal_velocity(0)
     time.sleep(2)

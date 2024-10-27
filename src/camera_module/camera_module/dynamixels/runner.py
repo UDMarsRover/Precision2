9d9c3c6009a9b6b5317b6c96c4d735eb3048a570
Ryan Kuederle <ryankuederle@icloud.com>
2024-10-27 19:06:57 -0400

    Multi-velocity

--- src/camera_module/camera_module/dynamixels/runner.py
+++ src/camera_module/camera_module/dynamixels/runner.py
@@ -4,9 +4,11 @@ import time
 motor = DynamixelMX('/dev/ttyAMA0', 1, 57600)
 motor.set_velocity_mode()
 print("SETTING VELOCITY MODE")
-motor.write_goal_velocity(40)
-# while True:
-#     motor.write_goal_velocity(10)
-#     time.sleep(2)
-#     motor.write_goal_velocity(0)
-#     time.sleep(2)
+# motor.write_goal_velocity()
+while True:
+    motor.write_goal_velocity(200)
+    time.sleep(2)
+    motor.write_goal_velocity(40)
+    time.sleep(2)
+    motor.write_goal_velocity(0)
+    time.sleep(2)

6bead1bd3a821c33fb7ea43f5f1398ebbb0136f5
Ryan Kuederle <ryankuederle@icloud.com>
2024-10-27 19:11:47 -0400

    Fixed to read position

--- src/camera_module/camera_module/dynamixels/runner.py
+++ src/camera_module/camera_module/dynamixels/runner.py
@@ -7,8 +7,8 @@ print("SETTING VELOCITY MODE")
 # motor.write_goal_velocity()
 while True:
     motor.write_goal_velocity(200)
-    while motor.read_present_velocity() < motor.get_max_position():
+    while motor.read_present_position() < motor.get_max_position():
         pass
     motor.write_goal_velocity(-100)
-    while motor.read_present_velocity() > motor.get_min_position():
+    while motor.read_present_position() > motor.get_min_position():
         pass

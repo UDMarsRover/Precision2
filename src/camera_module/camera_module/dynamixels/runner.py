3a969ad08243e273fe8c3e74f12726118500c4d2
Ryan Kuederle <ryankuederle@icloud.com>
2024-10-30 20:38:45 -0400

    Just activate

--- src/camera_module/camera_module/dynamixels/runner.py
+++ src/camera_module/camera_module/dynamixels/runner.py
@@ -2,15 +2,15 @@ from control import DynamixelMX
 import time
 
 motor = DynamixelMX('/dev/ttyAMA0', 1, 57600)
-motor.set_velocity_mode()
-print("SETTING VELOCITY MODE")
+# motor.set_velocity_mode()
+# print("SETTING VELOCITY MODE")
 # motor.write_goal_velocity()
-while True:
-    motor.write_goal_velocity(200)
-    while motor.read_present_position() < 3000:
-        print(motor.read_present_position())
-        pass
-    motor.write_goal_velocity(-100)
-    while motor.read_present_position() > 100:
-        print(motor.read_present_position())
-        pass
+# while True:
+#     motor.write_goal_velocity(200)
+#     while motor.read_present_position() < 3000:
+#         print(motor.read_present_position())
+#         pass
+#     motor.write_goal_velocity(-100)
+#     while motor.read_present_position() > 100:
+#         print(motor.read_present_position())
+#         pass

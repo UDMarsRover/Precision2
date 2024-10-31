d30d0296e6a87ccc206399c6bce612d2213e93c3
Ryan Kuederle <ryankuederle@icloud.com>
2024-10-30 20:33:10 -0400

    Dif limits

--- src/camera_module/camera_module/dynamixels/runner.py
+++ src/camera_module/camera_module/dynamixels/runner.py
@@ -7,10 +7,10 @@ print("SETTING VELOCITY MODE")
 # motor.write_goal_velocity()
 while True:
     motor.write_goal_velocity(200)
-    while motor.read_present_position() < 2000:
+    while motor.read_present_position() > 0:
         print(motor.read_present_position())
         pass
     motor.write_goal_velocity(-100)
-    while motor.read_present_position() > 0:
+    while motor.read_present_position() < 2000:
         print(motor.read_present_position())
         pass

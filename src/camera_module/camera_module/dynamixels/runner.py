3289d4666c70129b8fcc8bdda17e110520851182
Ryan Kuederle <ryankuederle@icloud.com>
2024-10-30 20:44:34 -0400

    longer delay

--- src/camera_module/camera_module/dynamixels/runner.py
+++ src/camera_module/camera_module/dynamixels/runner.py
@@ -4,6 +4,6 @@ import time
 motor = DynamixelMX('/dev/ttyAMA0', 1, 57600)
 while True:
     motor.write_goal_position(motor.get_max_position())
-    time.sleep(0.5)
+    time.sleep(1.5)
     motor.write_goal_position(motor.get_min_position())
-    time.sleep(0.5)
+    time.sleep(1.5)

fd71eaf71ea341f5d38512835c0fc135d74a6692
Ryan Kuederle <ryankuederle@icloud.com>
2025-07-11 01:59:07 -0400

    100 velocity

--- src/rover/rover/drive/bt_drive.py
+++ src/rover/rover/drive/bt_drive.py
@@ -30,7 +30,8 @@ class BTDrive:
             velocities = [self.right_velocity] * 3 + [self.left_velocity] * 3
             parsed_data = self.serial_conn.spin_once()
             print(f"Parsed data: {parsed_data}")
-            self.serial_conn.send_velocity_set(velocities)
+            self.serial_conn.send_velocity_set([100.0, 100.0, 100.0, 100.0, 100.0, 100.0])
+            # self.serial_conn.send_velocity_set(velocities)
             print(f"Setting velocities: Left: {self.left_velocity}, Right: {self.right_velocity}")
             self.ls_received = False  # Reset flag after processing
 

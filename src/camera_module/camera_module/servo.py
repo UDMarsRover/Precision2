36ceaca242c5aa8f90e272161191f052085af504
Ryan Kuederle <ryan@rkuederle.com>
2025-08-08 01:42:50 -0400

    adjust pulse lims

--- src/camera_module/camera_module/servo.py
+++ src/camera_module/camera_module/servo.py
@@ -11,8 +11,8 @@ class ServoNode(Node):
         self.servo = Servo(
             SERVO_PIN,
             initial_value=0,
-            min_pulse_width=0.0005,  # Try adjusting these
-            max_pulse_width=0.0025   # Try adjusting these
+            min_pulse_width=0.001,  # Try adjusting these
+            max_pulse_width=0.002   # Try adjusting these
         )
         self.last_position = None
         self.subscription = self.create_subscription(

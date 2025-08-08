f0bb82e855c1ce080977f75a02cf81b5391b3986
Ryan Kuederle <ryankuederle@icloud.com>
2025-08-08 13:51:45 -0600

    use sudo

--- src/rover/install/install.sh
+++ src/rover/install/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # install the bt service
-su
 
-cp bt_drive.service /etc/systemd/system/bt_drive.service
-systemctl daemon-reload
-systemctl enable bt_drive.service
-systemctl start bt_drive.service
+sudo cp bt_drive.service /etc/systemd/system/bt_drive.service
+sudo systemctl daemon-reload
+sudo systemctl enable bt_drive.service
+sudo systemctl start bt_drive.service

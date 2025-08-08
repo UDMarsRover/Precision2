431d1e13bd1f908f54434bbdbecbf943e26ce53c
Ryan Kuederle <ryankuederle@icloud.com>
2025-08-08 15:48:47 -0600

    revert wrong wait

--- src/rover/rover/drive/bt_drive.py
+++ src/rover/rover/drive/bt_drive.py
@@ -22,16 +22,8 @@ class BTDrive(Node):
         
         # Serial connection
         self.serial_conn = UDMRTMotorSerial(port=serial_port, baudrate=115200)
-
-        controller_connected = False
-        while not controller_connected:
-            try:
-                controller_connected = self.serial_conn.connect()
-                if controller_connected:
-                    self.get_logger().info("Connected to motor controller")
-            except SerialException as e:
-                self.get_logger().error(f"Failed to connect to motor controller: {e}")
-                time.sleep(3)
+        if not self.serial_conn.connect():
+            raise SerialException("Could not connect to motor controller")
         
         # Nintendo Pro Controller setup
         self.controller = NintendoProController()

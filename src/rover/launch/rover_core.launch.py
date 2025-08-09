873dc4df469d64a307a33b202cb6179daeeeaf1b
Ryan Kuederle <ryan@rkuederle.com>
2025-08-09 05:35:08 -0400

    fix node name

--- src/rover/launch/rover_core.launch.py
+++ src/rover/launch/rover_core.launch.py
@@ -14,7 +14,7 @@ def generate_launch_description():
             #         name="servo_node",
             #      ),
             Node(package="camera_module",
-                    executable="=dynamixel_node",
+                    executable="dynamixel_node",
                     name="dynamixel_node",
                  ),
         ]
